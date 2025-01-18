145287104fc081b30c1ce32840c473b802ac46fa
tunangoo <ngovantuan02092003@gmail.com>
2025-01-18 15:46:21 +0700

    add logo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { google } = require('googleapis');
 const express = require('express');
 const app = express();
@@ -7,9 +9,23 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
+let credentials;
+try {
+    // Thử đọc từ biến môi trường trước
+    if (process.env.GOOGLE_CREDENTIALS) {
+        credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+    } else {
+        // Nếu không có biến môi trường, đọc từ file
+        credentials = require('./credentials.json');
+    }
+} catch (error) {
+    console.error('Error loading credentials:', error);
+    process.exit(1);
+}
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: './credentials.json',
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    credentials,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 const spreadsheetId = '1VSL25sT6FQHjv1su9pfoNr6SmRigzJKHu03W8TVHX6Q';
@@ -47,7 +63,7 @@ setInterval(readSheet, 2000);
 // Đọc dữ liệu lần đầu khi khởi động server
 readSheet();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
